c133db6366ccf8db1f571aae258b7be67ade20ff
陈锴 <k_0520@qq.com>
2024-07-24 09:55:04 +0800

    save

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'live-video',
+  base: '/live-video',
   plugins: [
     vue(),
     UnoCSS(),
